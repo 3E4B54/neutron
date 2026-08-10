ad4815fba260e1750cb27c139bd9912c2e7bbe63
KT <37081025+3E4B54@users.noreply.github.com>
2026-08-10 03:55:28 +0000

    Switch GUI2 branch to the richer desktop

--- apps/plasmon/src/index.tsx
+++ apps/plasmon/src/index.tsx
@@ -1,12 +1,11 @@
 import { createRoot } from "react-dom/client";
-import { DesktopShell } from "./DesktopShell.tsx";
+import { DesktopShell2 } from "./gui2/DesktopShell2.tsx";
 import { installAppIconFallbacks } from "./iconFallback.ts";
 import "./style.scss";
-import "./desktop.scss";
-import "./desktop-overrides.scss";
+import "./gui2/desktop2.scss";
 
 installAppIconFallbacks();
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Root element not found");
-createRoot(container).render(<DesktopShell />);
+createRoot(container).render(<DesktopShell2 />);
