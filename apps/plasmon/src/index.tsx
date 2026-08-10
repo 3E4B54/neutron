8e13b9a4f1367901407f67501d8d73f32902679f
KT <37081025+3E4B54@users.noreply.github.com>
2026-08-10 16:40:31 +0000

    Boot Plasmon OS composition skeleton

--- apps/plasmon/src/index.tsx
+++ apps/plasmon/src/index.tsx
@@ -1,11 +1,10 @@
 import { createRoot } from "react-dom/client";
-import { DesktopShell2 } from "./gui2/DesktopShell2.tsx";
 import { installAppIconFallbacks } from "./iconFallback.ts";
+import { PlasmonOS } from "./os/PlasmonOS.tsx";
 import "./style.scss";
-import "./gui2/desktop2.scss";
 
 installAppIconFallbacks();
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Root element not found");
-createRoot(container).render(<DesktopShell2 />);
+createRoot(container).render(<PlasmonOS />);
