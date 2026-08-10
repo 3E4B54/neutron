6a419d384f3a896a3fffd2fd301992653624fbad
KT <37081025+3E4B54@users.noreply.github.com>
2026-08-10 03:07:30 +0000

    Layer desktop overlay polish

--- apps/plasmon/src/index.tsx
+++ apps/plasmon/src/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { DesktopShell } from "./DesktopShell.tsx";
 import "./style.scss";
 import "./desktop.scss";
+import "./desktop-overrides.scss";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Root element not found");
