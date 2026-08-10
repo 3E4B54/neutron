b6af2dfa7b8ad84d544f899e6a5746bbcabf2ef2
KT <37081025+3E4B54@users.noreply.github.com>
2026-08-10 03:39:56 +0000

    Enable resilient GUI app icons

--- apps/plasmon/src/index.tsx
+++ apps/plasmon/src/index.tsx
@@ -1,8 +1,11 @@
 import { createRoot } from "react-dom/client";
 import { DesktopShell } from "./DesktopShell.tsx";
+import { installAppIconFallbacks } from "./iconFallback.ts";
 import "./style.scss";
 import "./desktop.scss";
 import "./desktop-overrides.scss";
+
+installAppIconFallbacks();
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Root element not found");
